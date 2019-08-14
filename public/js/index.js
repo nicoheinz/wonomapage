4b98daad89442a6d688b52ae24bf44483d5a1dbe
nicoheinz <nicolasheinz.2014@gmail.com>
2019-08-14 10:38:16 -0300

    nico-index.js
    Agregado de movimientos a las imagenes de las secciones

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,63 @@
 $(document).ready(function(){
       
+    
     $('.nav-link').click(function(){
     
         $a=$(this).attr('id');
-        $("#seccion"+$a).hide().fadeIn(4000);
+        $("#seccion"+$a).hide().fadeIn(3000);
+
+    
     });
 
+
+
+    window.sr = ScrollReveal({ reset: true });
+    sr.reveal('.img1', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'left',
+    });
+
+    sr.reveal('.img2', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'right',
+    });
+
+    sr.reveal('.img3', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'bottom',
+    });
+
+    sr.reveal('.img4', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'left',
+    });
+
+    sr.reveal('.im5', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'right',
+    });
+
+
+    sr.reveal('.price-left', { 
+        duration: 2000,
+        distance: '100%',
+        origin: 'left',
+    });
+
+    sr.reveal('.price-center', { 
+        duration: 1000,
+        distance: '100%',
+        origin: 'bottom',
+    });
+
+    sr.reveal('.price-right', { 
+        duration: 2000,
+        distance: '100%',
+        origin: 'right',
+    }); 
 });
