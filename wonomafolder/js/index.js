fce178e5b7be52deac8c8cf36e06e7a4d8e0e8de
nicolas <nicolasheinz.2014@gmail.com>
2019-08-23 02:36:04 -0300

    Mejoras
    Diseño del formulario, agregado de banderas de países, creación del selector y mejoras varias.

--- wonomafolder/js/index.js
+++ wonomafolder/js/index.js
@@ -18,6 +18,76 @@ $(document).ready(function(){
         opacity: 0,
     });
 
+    $("#itemargetina").click(function(){
+        $(".pricedolar").hide();
+        $(".pricepesos").show();
+    });
 
+    $("#iteminternacional").click(function(){
+        $(".pricepesos").hide();
+        $(".pricedolar").show();
+    });
+
+    $("#direccion").click(function(){
+
+        $(".twLongform_FieldInput label").addClass("twLongform");
+        $(".twLongform_FieldInput label").show();
+    });
+
+    $('#direccion').on('focus blur', function(e) {
+        var placeholder = e.type == 'focus' ? '' : 'Direccion';
+        $(this).attr('placeholder', placeholder);
+    });
+
+    $("#tel").click(function(){
+
+        $(".twLongform_FieldInput label").addClass("twLongform");
+        $(".twLongform_FieldInput label").show();
+        $(this).attr('value', '+54');
+    });
+
+    $('#tel').on('focus blur', function(e) {
+        var placeholder = e.type == 'focus' ? '' : 'Teléfono';
+        $(this).attr('placeholder', placeholder);
+        $(this).attr('value', '');
+    });
+
+    $("#email").click(function(){
+
+        $(".twLongform_FieldInput label").addClass("twLongform");
+        $(".twLongform_FieldInput label").show();
+    });
+
+    $('#email').on('focus blur', function(e) {
+        var placeholder = e.type == 'focus' ? '' : 'Email';
+        $(this).attr('placeholder', placeholder);
+    });
+
+
+    $("#nombreContacto").click(function(){
+
+        $(".twLongform_FieldInput label").addClass("twLongform");
+        $(".twLongform_FieldInput label").show();
+    });
+
+    $('#nombreContacto').on('focus blur', function(e) {
+        var placeholder = e.type == 'focus' ? '' : 'Nombre Contacto';
+        $(this).attr('placeholder', placeholder);
+    });
+
+
+
+    $("#nomreEstablec").click(function(){
+
+        $(".twLongform_FieldInput label").addClass("twLongform");
+        $(".twLongform_FieldInput label").show();
+    });
+
+    $('#nomreEstablec').on('focus blur', function(e) {
+        var placeholder = e.type == 'focus' ? '' : 'Nombre de Establecimiento';
+        $(this).attr('placeholder', placeholder);
+    });
+
+    
     
 });
